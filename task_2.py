d06830b111bc2404527e990500e86e1dca9c5857
Bobstroitel3333 <svyatkovalev123@gmail.com>
2026-04-14 15:57:44 +0500

    control rab 1

---
+++ task_2.py
@@ -0,0 +1,13 @@
+#1 variant 
+#task 2
+def Sum(n):
+    if n==0:
+        return 0
+    def Mult(n)
+        if n==0:
+            return 0
+        return (1/(j+1)) * Mult(n - 1)
+    return Mult(n) + Sum(n - 1)
+n = int(input())
+result = Sum
+print(result)
