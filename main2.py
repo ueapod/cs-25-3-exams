94895585f720648f3f91b73db50437b1e2df288a
dima_stratonov <dimastratonov555@gmail.com>
2026-05-06 13:54:15 +0500

    lab 1 пересдача

--- main2.py
+++ main2.py
@@ -1,69 +1,31 @@
-class Movie:
-    def __init__(self, title, director, duration, year, genre, actors):
-        self._title = title
-        self._director = director
-        self._duration = duration
-        self._year = year
-        self._genre = genre
-        self._actors = actors
+# 1
+s = input("Введите строку: ")
 
-    # геттеры/сеттеры (минимум)
-    def get_title(self):
-        return self._title
+words = s.split()
 
-    def set_title(self, title):
-        self._title = title
+longest = ""
 
-    # методы
-    def get_age(self):
-        return 2026 - self._year  # просто руками
+for w in words:
+    if len(w) > len(longest):
+        longest = w
 
-    def is_classic(self):
-        return self.get_age() > 50
+print("Самое длинное слово:", longest)
+print("Длина слова:", len(longest)) 
 
-    def list_cast(self):
-        for a in self._actors:
-            print(a)
-
-    def get_info(self):
-        return self._title, self._director, self._year
-
-    def get_genre_info(self):
-        return "Жанр: " + self._genre
-
-    # перегрузки
-    def __str__(self):
-        return f"Movie: {self._title} by {self._director} ({self._year}) - {self._duration} мин"
-
-    def __lt__(self, other):
-        return self._duration < other._duration
-
-    def __add__(self, other):
-        return self._duration + other._duration
-
-    def __contains__(self, actor):
-        for a in self._actors:
-            if a == actor:
-                return True
-        return False
+# 2
+def summa(j):
+    if j == 1:
+        return (-1) / (1**2)
+    else:
+        return summa(j - 1) + ((-1)**j) / (j**2)
 
 
-class Documentary(Movie):
-    def __init__(self, title, director, duration, year, genre, actors, subject, facts):
-        super().__init__(title, director, duration, year, genre, actors)
-        self._subject = subject
-        self._facts = facts
+def P(k):
+    if k == 1:
+        return summa(1)
+    else:
+        return P(k - 1) * summa(k)
 
-    def is_classic(self):
-        return self.get_age() > 30  # изменили
 
-    def list_cast(self):
-        print("Участники:")
-        for a in self._actors:
-            print(a)
-
-    def get_facts_count(self):
-        return len(self._facts)
-
-    def get_genre_info(self):
-        return "Документалка про " + self._subject
+n = int(input("Введите n: "))
+print("P =", P(n))
